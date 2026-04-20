6cb57d8217a42c1a5ca9cace17de8244087a3b16
LightKhoza <u22647113@tuks.co.za>
2026-04-20 14:32:20 +0200

    session

---
+++ SeatAllocation/Controllers/SessionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SeatAllocation.Data;
+using SeatAllocation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SeatAllocation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public SessionController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Session session)
+        {
+            _context.Sessions.Add(session);
+            await _context.SaveChangesAsync();
+            return Ok(session);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _context.Sessions.ToListAsync());
+        }
+    }
+}
